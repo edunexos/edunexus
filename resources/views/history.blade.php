2fea932f42f4373741316185090b589428c0a81c
RobMan <63101014+seurob@users.noreply.github.com>
2024-06-15 23:57:38 +0200

    history

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -8,8 +8,6 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
-                <h1 class="text-2xl font-bold mb-6">History Game</h1>
-                <p class="text-gray-700 mb-4">History quiz</p>
                 <div id="app">
                     <history-quiz></history-quiz>
                 </div>
